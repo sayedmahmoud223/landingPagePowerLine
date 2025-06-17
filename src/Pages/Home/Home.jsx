2315eaaf014d51a4b6e6a015912e652d39fad970
sayedmahmoud223 <sbendary977@gmail.com>
2025-06-17 07:03:56 +0300

    remove gas

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,11 +6,11 @@ export default function Home() {
     <div id="Home" className="grid grid-cols-12 gap-4 p-8 mt-8">
       <div className="col-span-12 md:col-span-6 my-3 flex flex-col justify-around ">
         <p className="font-[Poppins] font-medium text-center md:text-right text-[40px] md:text-[45px] leading-[120%] tracking-[0px]">
-          مرحبًا بك في <span className="text-Primary-400 animate-pulse">باور لاين جاز للوقود</span>{" "}
+          مرحبًا بك في <span className="text-Primary-400 animate-pulse">باور لاين للوقود</span>{" "}
           الخبرة التي تعتمد عليها في إدارة وتشغيل محطات الوقود ومراكز الخدمة
         </p>
         <p className="font-inter font-normal text-[18px] leading-[150%] tracking-[0px] text-Neutral-600 my-6">
-          تُعَدّ باور لاين جاز للوقود شركة سعودية رائدة تأسست في عام 1446هـ (الموافق 2024م)، وتسعى إلى توفير حلول شاملة ومتميزة في مجال خدمات السيارات والمسافرين. تنطلق الشركة من رؤية واضحة ترتكز على تملُّك وتشغيل مراكز متكاملة تقع في مواقع استراتيجية، ما يضمن سهولة الوصول إليها ويوفّر تجربة سلسة للمسافرين على الطرق.
+          تُعَدّ باور لاين للوقود شركة سعودية رائدة تأسست في عام 1446هـ (الموافق 2024م)، وتسعى إلى توفير حلول شاملة ومتميزة في مجال خدمات السيارات والمسافرين. تنطلق الشركة من رؤية واضحة ترتكز على تملُّك وتشغيل مراكز متكاملة تقع في مواقع استراتيجية، ما يضمن سهولة الوصول إليها ويوفّر تجربة سلسة للمسافرين على الطرق.
         </p>
         <p className="font-inter font-normal text-[18px] leading-[150%] tracking-[0px] text-Neutral-600 my-6">
         تقدّم الشركة باقة واسعة من الخدمات، تشمل تزويد المركبات بالوقود بأنواعه المختلفة، وخدمات الصيانة الدوريّة والفنيّة.
